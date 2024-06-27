11b6958b48a660431e4f7ec9d6ddee4428b7d582
Domenico Luciani <domenicoleoneluciani@gmail.com>
2024-06-27 12:26:23 +0200

    fix: adjust order

--- hash/simple.go
+++ hash/simple.go
@@ -2,13 +2,13 @@ package hash
 
 import "hash/fnv"
 
+func GetInstanceFromKey(key string, numberOfInstances int) int {
+	hash := hashId(key)
+	return int(hash) % int(numberOfInstances)
+}
+
 func hashId(id string) uint32 {
 	h := fnv.New32a()
 	h.Write([]byte(id))
 	return h.Sum32()
-}
-
-func GetInstanceFromKey(key string, numberOfInstances int) int {
-	hash := hashId(key)
-	return int(hash) % int(numberOfInstances)
 }
