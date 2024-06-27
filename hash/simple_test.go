4c22b665398fa66f6ec91ef743c382050daf248c
Domenico Luciani <domenicoleoneluciani@gmail.com>
2024-06-27 12:25:52 +0200

    fix: adjust test name

--- hash/simple_test.go
+++ hash/simple_test.go
@@ -11,7 +11,7 @@ func TestConsistent(t *testing.T) {
 		assert.Equal(t, hashId("tYxdBGXBdjPx4Bus7Nsbvya99JWCreyR"), uint32(1082065427))
 	})
 
-	t.Run("Given a certain ID and minio instances, it should return the correspondent minio instance", func(t *testing.T) {
+	t.Run("Given a certain ID and number of instances, it should return the correspondent instance", func(t *testing.T) {
 		assert.Equal(t, GetInstanceFromKey("tYxdBGXBdjPx4Bus7Nsbvya99JWCreyR", 3), 2)
 	})
 }
